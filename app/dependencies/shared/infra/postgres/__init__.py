8f6b963dfda09ef77b0549470a600a01bd5758e4
Abhijeet-1409 <abhijeetjoy14@@gmail.com>
2026-07-29 12:06:53 +0530

    feat(postgres): add User and user-related functions to module exports

--- app/dependencies/shared/infra/postgres/__init__.py
+++ app/dependencies/shared/infra/postgres/__init__.py
@@ -2,6 +2,7 @@ from .base import Base
 from .call_log import CallLog, save_call_log
 from .transcript import Transcript, save_transcript
 from .tool_log import ToolLog, save_tool_log
+from .user import User, create_user, get_user
 from .knowledge_base import KnowledgeBase
 from .database import db_close, db_init
 
@@ -10,6 +11,9 @@ __all__ = [
     "CallLog",
     "Transcript",
     "ToolLog",
+    "User",
+    "create_user",
+    "get_user",
     "KnowledgeBase",
     "save_call_log",
     "save_transcript",
