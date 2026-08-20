a08fd5d9371f82f6f2aa671c7e4d21403ca39b5e
Abhijeet-1409 <abhijeetjoy14@@gmail.com>
2026-08-20 19:03:24 +0530

    feat(worker): initialize worker domain module with tools and system prompts

---
+++ app/services/worker/domain/__init__.py
@@ -0,0 +1,9 @@
+from . import tools
+from .system_prompt import INBOUND_PROMPT, OUTREACH_PROMPT, DEFAULT_PROMPT
+
+__all__ = [
+    "tools",
+    "INBOUND_PROMPT",
+    "OUTREACH_PROMPT",
+    "DEFAULT_PROMPT",
+]
