5c4b07b3a865ffacb5c899e0b709985ecdb90f9e
Abhijeet-1409 <abhijeetjoy14@@gmail.com>
2026-08-31 15:14:23 +0530

    refactor(postgres): rename KnowledgeBase to KnowledgeChunk for consistency

--- app/dependencies/shared/infra/postgres/__init__.py
+++ app/dependencies/shared/infra/postgres/__init__.py
@@ -3,7 +3,7 @@ from .call_log import CallLog, save_call_log
 from .transcript import Transcript, save_transcript
 from .tool_log import ToolLog, save_tool_log
 from .user import User, create_user, get_user
-from .knowledge_base import KnowledgeBase
+from .knowledge_chunk import KnowledgeChunk
 from .database import db_close, db_init
 from .ticket import Ticket
 from .contact import Contact
@@ -18,7 +18,7 @@ __all__ = [
     "Ticket",
     "create_user",
     "get_user",
-    "KnowledgeBase",
+    "KnowledgeChunk",
     "save_call_log",
     "save_transcript",
     "save_tool_log",
